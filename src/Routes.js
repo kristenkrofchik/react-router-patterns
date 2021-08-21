5cbdd1a93b280e710ad5e5dbeb9e245c6b0bedea
Kristen Krofchik <k.krofchik@gmail.com>
2021-08-21 17:14:07 -0500

    routes done

--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect, useParams } from 'react-router-dom';
 import DogList from './DogList';
 import DogDetails from './DogDetails';
 
 function Routes({dogs}) {
     return (
         <Switch>
-            <Route exact path='/dogs'><DogList dogs={dogs} /></Route>
+            <Route exact path='/dogs'>
+                <DogList dogs={dogs} />
+            </Route>
+            <Route path='/dogs/:name'>
+                <FilterDogDetails dogs={dogs} />
+            </Route>
             <Redirect to='/dogs' />
         </Switch>
     )
+}
+
+function FilterDogDetails({dogs}) {
+    const {name} = useParams();
+
+    if(name) {
+        const thisDog = dogs.find(
+            dog => dog.name.toLowerCase() === name.toLowerCase()
+        );
+        return <DogDetails dog={thisDog} />;
+    }
+    return null;
 }
 
 export default Routes;

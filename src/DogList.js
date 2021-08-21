83a55a2a50818acfce490e2efa6b44a81db91f64
Kristen Krofchik <k.krofchik@gmail.com>
2021-08-21 17:21:52 -0500

    doglist done

--- src/DogList.js
+++ src/DogList.js
@@ -1,8 +1,24 @@
 import React from "react";
-import DogDetails from "./DogDetails";
+import { Link } from 'react-router-dom';
 
-function DogList() {
-
+function DogList({dogs}) {
+    return (
+        <div className='DogList'>
+            <h1>
+                These are the Dawgs! Click on a Pup for More Info!
+            </h1>
+            <ul>
+                {dogs.map(d => (
+                    <li>
+                        <img src={d.src} alt={d.name} />
+                        <h3>
+                            <Link to={`/dogs/${d.name.toLowerCase()}`}>{d.name}</Link>
+                        </h3>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default DogList;

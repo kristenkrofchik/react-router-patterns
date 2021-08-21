5dc27c44016cdc5be51dde6ee7cb20a854d99113
Kristen Krofchik <k.krofchik@gmail.com>
2021-08-21 17:26:01 -0500

    dogdetails done

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { Link, Redirect } from 'react-router-dom';
 
-function DogDetails() {
-    const { name } = useParams();
-    const [src, setSrc] = useState(null);
+function DogDetails({dog}) {
+    if(!dog) return <Redirect to='/dogs' />
 
-    useEffect(() => {
-        async function fetchPhoto(dogName) {
-            setSrc()
-        }
-    })
+    return (
+        <div className='DogDetails'>
+            <img src={dog.src} alt={dog.name} />
+            <h2>{dog.name}</h2>
+            <h3>{dog.age} years old</h3>
+            <ul>
+                {dog.facts.map((fact, i) => (
+                    <li key={i}>{fact}</li>
+                ))}
+            </ul>
+            <Link to='/dogs'>Back to Dog List</Link>
+        </div>
+    );
 }
+
+export default DogDetails;
